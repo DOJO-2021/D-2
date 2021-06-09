7350a10e5f0647f000b0b5879ead8a96249017f6
dojo2021-y-yokota <yokota-yui@2016@seplus.onmicrosoft.com>
2021-06-09 15:59:57 +0900

    ResistServlet.java

--- Knowledge Holder/src/servlet/ResistServlet.java
+++ Knowledge Holder/src/servlet/ResistServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,7 +30,6 @@ public class ResistServlet extends HttpServlet {
 		dispatcher.forward(request, response);
 	}
 
-	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -39,4 +40,5 @@ public class ResistServlet extends HttpServlet {
 			return;
 	}
 
+}
 }
