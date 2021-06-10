a4adb1a7bc66d8454633fceb5141af8f03762055
DOJO2021-r-kageyama <kageyama-r@seplus2016.onmicrosoft.com>
2021-06-10 16:02:17 +0900

    #22途中経過

--- KnowledgeHolder/WebContent/js/question_list.js
+++ KnowledgeHolder/WebContent/js/question_list.js
@@ -1,3 +1,21 @@
 /**
- * 
+ *
  */
+ $(function(){
+      // ボタン押下時の処理
+      $('#btn').on('click',function(){
+        $.ajax({
+          url: "AjaxServlet",
+          type: "GET",
+          data: {num : $("#text1").val()}
+        }).done(function (result) {
+          // 通信成功時のコールバック
+          $("#text1").val(result);
+        }).fail(function () {
+          // 通信失敗時のコールバック
+          alert("読み込み失敗");
+        }).always(function (result) {
+          // 常に実行する処理
+        });
+      });
+});
