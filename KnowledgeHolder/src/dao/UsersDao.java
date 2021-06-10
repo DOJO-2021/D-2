19be83d17643ce7b6e7c4051ec1078a712bfbd5b
dojo2021-y-yokota <yokota-yui@2016@seplus.onmicrosoft.com>
2021-06-10 16:47:04 +0900

    dao

--- KnowledgeHolder/src/dao/UsersDao.java
+++ KnowledgeHolder/src/dao/UsersDao.java
@@ -1,5 +1,63 @@
 package dao;
 
-public class UsersDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class UsersDao {
+	// ログインできるならtrueを返す
+		public boolean isLoginOK(String user_mail, String user_pw) {
+			// connは接続？
+			Connection conn = null;
+			boolean loginResult = false;
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:C:/pleiades/workspace/D-2/KnowledgeHolder/data/knowledge", "sa", "pass");
+
+				// SELECT文を準備する
+				String sql = "select count(*) from Users where user_mail = ? and user_pw = ?";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+				pStmt.setString(1, user_mail);
+				pStmt.setString(2, user_pw);
+
+				// SELECT文を実行し、結果表を取得する
+				ResultSet rs = pStmt.executeQuery();
+
+				// ユーザーIDとパスワードが一致するユーザーがいたかどうかをチェックする
+				rs.next();
+				if (rs.getInt("count(*)") == 1) {
+					loginResult = true;
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+				loginResult = false;
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				loginResult = false;
+				// JDBCが見つからなかったときなど
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+						loginResult = false;
+					}
+				}
+			}
+
+			// 結果を返す
+			return loginResult;
+		}
 }
