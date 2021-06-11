e900bd12a61ee6b2f162a93f307378b120a06935
dojo2021-y-yokota <yokota-yui@2016@seplus.onmicrosoft.com>
2021-06-11 15:25:37 +0900

    rollback.

--- KnowledgeHolder/src/dao/QuestionsDao.java
+++ KnowledgeHolder/src/dao/QuestionsDao.java
@@ -85,7 +85,7 @@ public class QuestionsDao {
 
 	//名前または住所を指定して検索する。
 	//nullまたは空文字の場合は条件指定しない。
-	public List<Question> selectByQue_titleOrQue_contents(String que_title, String que_contents){
+	public List<Question> selectByQue_categoryOrQue_titleOrQue_contents(String que_category, String que_title, String que_contents){
 		Connection conn = null;
 		List<Question> questionList = new ArrayList<Question>();
 		try {
@@ -93,6 +93,8 @@ public class QuestionsDao {
 			Class.forName("org.h2.Driver");
 			// データベースに接続する
 			conn = DriverManager.getConnection("jdbc:h2:C:/pleiades/workspace/D-2/KnowledgeHolder/data/knowledge", "sa", "pass");
+			//質問カテゴリ検索の有無を保持する。nullでも空文字でもなければ有効値
+			boolean hasQue_category = que_category != null && !que_category.equals("");
 			//質問タイトル検索の有無を保持する。nullでも空文字でもなければ有効値
 			boolean hasQue_title = que_title != null && !que_title.equals("");
 			//質問内容検索の有無を保持する。nullでも空文字でもなければ有効値
@@ -100,10 +102,17 @@ public class QuestionsDao {
 			//追加した条件数を保持する変数
 			int added = 0;
 			// SQL文を準備する
-			String sql = "select QUE_TITLE, QUE_CONTENTS from QUESTIONS ";
+			String sql = "select QUE_CATEGORY, QUE_TITLE, QUE_CONTENTS from QUESTIONS ";
 			//名前、または住所の指定があれば条件検索を行う
-			if(hasQue_title || hasQue_contents ) {
+			if(hasQue_category || hasQue_title || hasQue_contents) {
 				sql += "WHERE ";
+			}
+			if(hasQue_category) {
+				if(added > 0) {
+					sql += "AND ";
+				}
+				sql += "QUE_CATEGORY LIKE ? ";
+				added ++;
 			}
 			if(hasQue_title) {
 				if(added > 0) {
@@ -125,6 +134,10 @@ public class QuestionsDao {
 			//?がいくつあるかわからないので、カウンタで管理する。
 			//カウンタの初期化
 			added = 0;
+			if(hasQue_category) {
+				added ++;
+				pStmt.setString(added, "%" + que_category + "%");
+			}
 			if(hasQue_title) {
 				added ++;
 				pStmt.setString(added, "%" + que_title + "%");
@@ -138,79 +151,9 @@ public class QuestionsDao {
 			// 結果表をコレクションにコピーする
 			while (rs.next()) {
 				Question question = new Question(
+						rs.getString("QUE_CATEGORY"),
 						rs.getString("QUE_TITLE"),
 						rs.getString("QUE_CONTENTS")
-				);
-				questionList.add(question);
-			}
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-			questionList = null;
-		}
-		catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			questionList = null;
-		}
-		finally {
-			// データベースを切断
-			if (conn != null) {
-				try {
-					conn.close();
-				}
-				catch (SQLException e) {
-					e.printStackTrace();
-					questionList = null;
-				}
-			}
-		}
-		// 結果を返す
-		return questionList;
-	}
-
-	public List<Question> selectByQue_category(String que_category){
-		Connection conn = null;
-		List<Question> questionList = new ArrayList<Question>();
-		try {
-			// JDBCドライバを読み込む
-			Class.forName("org.h2.Driver");
-			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:C:/pleiades/workspace/D-2/KnowledgeHolder/data/knowledge", "sa", "pass");
-			//質問タイトル検索の有無を保持する。nullでも空文字でもなければ有効値
-			boolean hasQue_category = que_category != null && !que_category.equals("");
-
-			//追加した条件数を保持する変数
-			int added = 0;
-			// SQL文を準備する
-			String sql = "select QUE_CATEGORY from QUESTIONS ";
-			//名前、または住所の指定があれば条件検索を行う
-			if(hasQue_category &&) {
-				sql += "WHERE ";
-			}
-			if(hasQue_category) {
-				if(added > 0) {
-					sql += "AND ";
-				}
-				sql += "QUE_TITLE LIKE ? ";
-				added ++;
-			}
-			//ここまででSQL確定。ただし、パラメータ?の数は状況によって変わる。
-			PreparedStatement pStmt = conn.prepareStatement(sql);
-			// SQL文を完成させる
-			//?がいくつあるかわからないので、カウンタで管理する。
-			//カウンタの初期化
-			added = 0;
-			if(hasQue_category) {
-				added ++;
-				pStmt.setString(added, "%" + que_category + "%");
-			}
-
-			// SQL文を実行し、結果表を取得する
-			ResultSet rs = pStmt.executeQuery();
-			// 結果表をコレクションにコピーする
-			while (rs.next()) {
-				Question question = new Question(
-						rs.getString("QUE_CATEGORY")
 				);
 				questionList.add(question);
 			}
@@ -410,6 +353,55 @@ public class QuestionsDao {
 		// 結果を返す
 		return result;
 	}
+
+	// 引数questionで指定されたレコードを更新し、成功したらtrueを返す
+		public boolean update_status(Question question) {
+			Connection conn = null;
+			boolean result = false;
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:C:/pleiades/workspace/D-2/KnowledgeHolder/data/knowledge", "sa", "pass");
+
+				// SQL文を準備する
+				String sql = "update QUESTIONS set f_tag=? where que_id=?";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				pStmt.setInt(1, question.getF_tag());
+				pStmt.setInt(2, question.getQue_id());
+
+				// SQL文を実行する
+				if (pStmt.executeUpdate() == 1) {
+					result = true;
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			// 結果を返す
+			return result;
+		}
+
+
 
 	// 引数numberで指定されたレコードを削除し、成功したらtrueを返す
 			public boolean delete(int que_id) {
