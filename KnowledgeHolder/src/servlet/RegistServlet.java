eb91a76a2e7d1d06ab0a8f383758e3ae5112be63
dojo2021-norikawa <norikawa@seplus2016.onmicrosoft.com>
2021-06-11 16:36:47 +0900

    registservlet

--- KnowledgeHolder/src/servlet/RegistServlet.java
+++ KnowledgeHolder/src/servlet/RegistServlet.java
@@ -17,6 +17,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+import dao.QuestionsDao;
+import model.Question;
+
 
 
 @WebServlet("/RegistServlet")
@@ -54,13 +57,13 @@ public class RegistServlet extends HttpServlet {
 
 
 		//アップされたファイル名（forループの中で取得）
-		String uploadFileName = "";
+		//String uploadFileName = "";
 		//画像を保存するパス
 		//ここでは、PCのデスクトップに保存して確認できるようにしている
-		String uploadFolder = "C:\\Users\\sep-other\\Desktop\\ImageSample\\ImageSample\\WebContent\\img\\uploaded\\";
+		//String uploadFolder = "C:\\Users\\sep-other\\Desktop\\ImageSample\\ImageSample\\WebContent\\img\\uploaded\\";
 
 		//名前が決まってから画像を処理するために、Partを保持しておく
-		Part imgPart = null;
+		//Part imgPart = null;
 
 		for(Part part:parts){ //partsから１つずつ取り出す
 			String contentType = part.getContentType();
@@ -76,37 +79,72 @@ public class RegistServlet extends HttpServlet {
 				} catch (IOException e) {
 					throw new RuntimeException(e);
 				}
-			}else{
+			}/*else{
 				//アップロードされたファイルの処理
 
 				uploadFileName = this.getFileName(part);
 				//実際には、ファイル名を商品IDなどに置き換えることになる（同一ファイル名対策）
 				//ここだけコピペじゃなく、自分で実装すること
-				part.write(uploadFolder + uploadFileName);
+				part.write(uploadFolder + uploadFileName );
 
 				//みなさんのシステムでは、AIを使っている場合、名前がまだ決まらない
-				imgPart = part;
-			}
+				//imgPart = part;
+			}*/
 		}
 
 		//値の取り方
 		//getParameter()の代わりにmapから、画面のHTMLで設定したname属性で取得する
-		String que_title = map.get("que-title");
-		String que_category = map.get("que-category");
-		String que_contents = map.get("que-contents");
+		String que_title = map.get("que_title");
+		String que_category = map.get("que_category");
+		String que_contents = map.get("que_contents");
+		//String que_file = (uploadFolder + uploadFileName);
 
-		//ここでdaoを使って１行作成する
+		//登録処理を行う
+		 QuestionsDao bDao = new  QuestionsDao();
+		 if (bDao.insert(new Question(0, que_category, que_title, que_contents, "que_file",0,0,0,"" ))) {
 
+			 //登録成功時検索ページにフォワードする
+			 RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/search.jsp");
+			 dispatcher.forward(request, response);
+		 }
+		 else {
+			//登録失敗時検索ページにリダイレクトする
+			 response.sendRedirect("/KnowledgeHolder/RegistServlet");
+			 return;
+		 }
 	}
 
 
-
-	private String getFileName(Part part) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+	/* ****************************** */
+	/*
+	//更新画面の場合
+	BookDAO dao = new BookDAO();
+	//更新対象行を取得する
+	Book b = dao.selectById(id);
+	// path /img/uploaded/湖.jpg
+	//画面上の値で上書き
+	b.setTitle(item1);
+	b.setPrice(item2);
+	//画像パスが「あれば」更新
+	//ただし、画像のファイル名＝idである場合は、そもそも更新が必要ない
+	if(imgPart != null){
+		b.setFilePath(uploadFolder + uploadFileName);
 	}
+	*/
 
-
+	/*private String getFileName(Part part) {
+	    String name = null;
+	    for (String dispotion : part.getHeader("Content-Disposition").split(";")) {
+	        if (dispotion.trim().startsWith("filename")) {
+	            name = dispotion.substring(dispotion.indexOf("=") + 1).replace("\"", "").trim();
+	            name = name.substring(name.lastIndexOf("\\") + 1);
+	            break;
+	        }
+	    }
+	    return name;
+	}*/
 
 
 }
+
+
