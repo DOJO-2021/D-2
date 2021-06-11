98f3256e807a567a2a192a9da8706623245edcc0
DOJO2021-r-kageyama <kageyama-r@seplus2016.onmicrosoft.com>
2021-06-11 14:07:04 +0900

    #29　完了未完了用クラス追加

--- KnowledgeHolder/src/dao/QuestionsDao.java
+++ KnowledgeHolder/src/dao/QuestionsDao.java
@@ -354,6 +354,55 @@ public class QuestionsDao {
 		return result;
 	}
 
+	// 引数questionで指定されたレコードを更新し、成功したらtrueを返す
+		public boolean update_status(Question question) {
+			Connection conn = null;
+			boolean result = false;
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:C:/pleiades/workspace/D-2/KnowledgeHolder/data/knowledge", "sa", "pass");
+
+				// SQL文を準備する
+				String sql = "update QUESTIONS set f_tag=? where que_id=?";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				pStmt.setInt(1, question.getF_tag());
+				pStmt.setInt(2, question.getQue_id());
+
+				// SQL文を実行する
+				if (pStmt.executeUpdate() == 1) {
+					result = true;
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			// 結果を返す
+			return result;
+		}
+
+
+
 	// 引数numberで指定されたレコードを削除し、成功したらtrueを返す
 			public boolean delete(int que_id) {
 				Connection conn = null;
