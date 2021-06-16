bac3677225f1b9a429b91f1ff4ee20ddf35c5409
DOJO2021-r-kageyama <kageyama-r@seplus2016.onmicrosoft.com>
2021-06-16 13:37:46 +0900

    #58 #60

--- KnowledgeHolder/src/dao/QuestionsAnswersDao.java
+++ KnowledgeHolder/src/dao/QuestionsAnswersDao.java
@@ -12,7 +12,7 @@ import model.QuestionAnswer;
 
 public class QuestionsAnswersDao {
 
-	//一覧表示
+	/*//一覧表示
 	public List<QuestionAnswer> allselect(QuestionAnswer param) {
 		Connection conn = null;
 		List<QuestionAnswer> allList = new ArrayList<QuestionAnswer>();
@@ -24,7 +24,7 @@ public class QuestionsAnswersDao {
 			conn = DriverManager.getConnection("jdbc:h2:C:/pleiades/workspace/D-2/KnowledgeHolder/data/KnowledgeHolder", "sa", "pass");
 
 			// SQL文を準備する
-			String sql = "select q.que_id, q.que_category, q.que_title, q.que_contents, q.que_file, q.user_id, q. f_tag, q.que_count, q.que_date, a.ans_id, a.que_id, a.ans_contents, a.ans_file, a.ans_date, u.user_name	FROM QUESTIONS  q INNER JOIN ANSWERS a ON q.QUE_ID  = a.QUE_ID INNER JOIN USERS u ON q.user_id  = u.user_id WHERE q.que_id = ?, q.user_id = ?";
+			String sql = "select q.que_id, q.que_category, q.que_title, q.que_contents, q.que_file, q.user_id, q. f_tag, q.que_count, q.que_date, a.ans_id, a.que_id, a.ans_contents, a.ans_file, a.ans_date, u.user_name	FROM QUESTIONS  q INNER JOIN ANSWERS a ON q.QUE_ID  = a.QUE_ID INNER JOIN USERS u ON q.user_id  = u.user_id where q.que_id = ? and q.user_id = ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
@@ -81,5 +81,143 @@ public class QuestionsAnswersDao {
 			// 結果を返す
 			return allList;
 		}
+	*/
 
+	//質問表示
+	public List<QuestionAnswer> questions(QuestionAnswer param) {
+		Connection conn = null;
+		List<QuestionAnswer> queList = new ArrayList<QuestionAnswer>();
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:C:/pleiades/workspace/D-2/KnowledgeHolder/data/KnowledgeHolder", "sa", "pass");
+
+			// SQL文を準備する
+			String sql = "select q.que_id, q.que_category, q.que_title, q.que_contents, q.que_file, q.user_id, q. f_tag, q.que_count, q.que_date, u.user_name	FROM QUESTIONS q INNER JOIN USERS u ON q.user_id  = u.user_id where q.que_id = ? and q.user_id = ?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setInt(1, param.getQue_id());
+			pStmt.setInt(2, param.getUser_id());
+
+
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする
+			while (rs.next()) { //データーがある限り
+				QuestionAnswer que = new QuestionAnswer(
+				rs.getInt("que_id"),
+				rs.getString("que_category"),
+				rs.getString("que_title"),
+				rs.getString("que_contents"),
+				rs.getString("que_file"),
+				rs.getInt("user_id"),
+				rs.getInt("f_tag"),
+				rs.getInt("que_count"),
+				rs.getString("que_date"),
+				0,
+				"",
+				"",
+				"",
+				rs.getString("user_name")
+				);
+				queList.add(que);
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+				queList = null;
+			}
+			//データベースがない場合のエラー
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				queList = null;
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+						queList = null;
+					}
+				}
+			}
+
+			// 結果を返す
+			return queList;
+		}
+
+	//回答表示
+		public List<QuestionAnswer> answers(QuestionAnswer param) {
+			Connection conn = null;
+			List<QuestionAnswer> ansList = new ArrayList<QuestionAnswer>();
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:C:/pleiades/workspace/D-2/KnowledgeHolder/data/KnowledgeHolder", "sa", "pass");
+
+				// SQL文を準備する
+				String sql = "select a.ans_id, a.ans_contents, a.ans_file, a.user_id, a.ans_date, u.user_name	FROM ANSWERS a INNER JOIN USERS u ON a.user_id  = u.user_id where a.que_id = ?;";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				pStmt.setInt(1, param.getQue_id());
+
+				// SQL文を実行し、結果表を取得する
+				ResultSet rs = pStmt.executeQuery();
+
+				// 結果表をコレクションにコピーする
+				while (rs.next()) { //データーがある限り
+					QuestionAnswer ans = new QuestionAnswer(
+					0,
+					"",
+					"",
+					"",
+					"",
+					rs.getInt("user_id"),
+					0,
+					0,
+					"",
+					rs.getInt("ans_id"),
+					rs.getString("ans_contents"),
+					rs.getString("ans_file"),
+					rs.getString("ans_date"),
+					rs.getString("user_name")
+					);
+					ansList.add(ans);
+					}
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					ansList = null;
+				}
+				//データベースがない場合のエラー
+				catch (ClassNotFoundException e) {
+					e.printStackTrace();
+					ansList = null;
+				}
+				finally {
+					// データベースを切断
+					if (conn != null) {
+						try {
+							conn.close();
+						}
+						catch (SQLException e) {
+							e.printStackTrace();
+							ansList = null;
+						}
+					}
+				}
+
+				// 結果を返す
+				return ansList;
+			}
 }
--- KnowledgeHolder/src/test/QuestionsAnswersDaoTest.java
+++ KnowledgeHolder/src/test/QuestionsAnswersDaoTest.java
@@ -8,7 +8,7 @@ import model.QuestionAnswer;
 public class QuestionsAnswersDaoTest {
 	public static void main(String[] args) {
 		QuestionsAnswersDao dao = new QuestionsAnswersDao();
-
+		/*
 		// allselect()のテスト　que_id=1 User_id=1のリストを検索
 		System.out.println("---------- allselect()のテスト----------");
 		List<QuestionAnswer> allList = dao.allselect(new QuestionAnswer(1, "", "","", "", 1, 0, 0,"", 0, "", "", "", ""));
@@ -28,5 +28,35 @@ public class QuestionsAnswersDaoTest {
 			System.out.println(all.getAns_date());
 			System.out.println(all.getUser_name());
 		}
+		*/
+
+		// question()のテスト　que_id=1 User_id=1のリストを検索
+		System.out.println("---------- question()のテスト----------");
+		List<QuestionAnswer> queList = dao.questions(new QuestionAnswer(2, "", "","", "", 2, 0, 0,"", 0, "", "", "", ""));
+		for (QuestionAnswer que : queList) {
+			System.out.println(que.getQue_id());
+			System.out.println(que.getQue_category());
+			System.out.println(que.getQue_title());
+			System.out.println(que.getQue_contents());
+			System.out.println(que.getQue_file());
+			System.out.println(que.getUser_id());
+			System.out.println(que.getF_tag());
+			System.out.println(que.getQue_count());
+			System.out.println(que.getQue_date());
+			System.out.println(que.getUser_name());
+		}
+
+		// answers()のテスト　que_id=1 User_id=1のリストを検索
+		System.out.println("---------- answers()のテスト----------");
+		List<QuestionAnswer> ansList = dao.answers(new QuestionAnswer(2, "", "","", "", 1, 0, 0,"", 0, "", "", "", ""));
+		for (QuestionAnswer ans : ansList) {
+			System.out.println(ans.getUser_id());
+			System.out.println(ans.getAns_id());
+			System.out.println(ans.getAns_contents());
+			System.out.println(ans.getAns_file());
+			System.out.println(ans.getAns_date());
+			System.out.println(ans.getUser_name());
+		}
+
 	}
 }
